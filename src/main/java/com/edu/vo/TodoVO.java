ed1716a5dc86068e02fdfd76b3a6fc7818b00f4e
이시은 <tldms0137@naver.com>
2021-07-27 16:07:56 +0900

    등록일시 수정일시 추가, 달력 추가

--- src/main/java/com/edu/vo/TodoVO.java
+++ src/main/java/com/edu/vo/TodoVO.java
@@ -1,11 +1,15 @@
 package com.edu.vo;
 
+import java.sql.Date;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class TodoVO {
 	private Integer no;
 	private String list;
+	private Date reg_date;		
+	private Date update_date;	
 	
 	public Integer getNo() {
 		return no;
@@ -19,6 +23,18 @@ public class TodoVO {
 	public void setList(String list) {
 		this.list = list;
 	}
-
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public Date getUpdate_date() {
+		return update_date;
+	}
+	public void setUpdate_date(Date update_date) {
+		this.update_date = update_date;
+	}
+	
 	
 }
